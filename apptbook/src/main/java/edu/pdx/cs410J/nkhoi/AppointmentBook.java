c21d5c500adf54dda78b51f5f8fda5af699024bb
klhexpress <47010729+klhexpress@users.noreply.github.com>
2019-07-12 01:18:16 -0700

    Partially finish Project 1

---
+++ apptbook/src/main/java/edu/pdx/cs410J/nkhoi/AppointmentBook.java
@@ -0,0 +1,89 @@
+package edu.pdx.cs410J.nkhoi;
+
+import edu.pdx.cs410J.AbstractAppointment;
+import edu.pdx.cs410J.AbstractAppointmentBook;
+import java.util.Collection;
+import java.util.ArrayList;
+
+/**
+ * This class is represents an <code>AppointmentBook</code>.
+ */
+public class AppointmentBook extends AbstractAppointmentBook {
+    private String owner;
+    private ArrayList<AbstractAppointment> list;
+
+    /**
+     * Creates an empty <code>AppointmentBook</code>
+     */
+    public AppointmentBook() {
+        list = null;
+    }
+
+    /**
+     * Creates a new <code>AppointmentBook</code>
+     * @param name
+     *        The name of the AppointmentBook's owner
+     * @param target
+     *        The appointment which you want to add to the AppointmentBook
+     */
+    public AppointmentBook(String name, Appointment target) {
+        owner = name;
+        list = new ArrayList<AbstractAppointment>();
+        list.add(target);
+    }
+
+    /**
+     * @return the name of the AppointmentBook's owner
+     */
+    @Override
+    public String getOwnerName() {
+        return owner;
+    }
+
+    /**
+     * @return the appointments in the AppointmentBook
+     */
+    @Override
+    public Collection getAppointments() {
+        return list;
+    }
+
+    /**
+     * Wrapper function help to add an appointment to the AppointmentBook
+     * @param target
+     *        The appointment added to the AppointmentBook
+     */
+    @Override
+    public void addAppointment(AbstractAppointment target) {
+        list = AddingAppointment(target);
+    }
+
+    /**
+     *The function adding an appointment to the AppointmentBook
+     * @param target
+     *        The appointment added to the AppointmentBook
+     * @return the list of appointment in the AppointmentBook
+     */
+    private ArrayList<AbstractAppointment> AddingAppointment(AbstractAppointment target) {
+        if (list == null) {
+            list = new ArrayList<AbstractAppointment>();
+            list.add(target);
+        } else {
+            list.add(target);
+        }
+        return list;
+    }
+
+    /**
+     *Display all the appointments in the AppointmentBook
+     */
+    public void display() {
+        if (list == null) {
+            throw new NullPointerException();
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i).toString());
+            }
+        }
+    }
+}
